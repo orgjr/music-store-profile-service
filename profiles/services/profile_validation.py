fbfbc3341767d16d09eb50a9331239fb2967449c
orgjr <orgj@protonmail.com>
2026-07-24 18:08:13 -0300

    feat: add profile input validation

---
+++ profiles/services/profile_validation.py
@@ -0,0 +1,45 @@
+def validate_alphanumeric(attr, value: str):
+    if not all(pace.isalnum() or pace.isspace() for pace in value) is True:
+        raise ValueError(f"{attr} has invalids characters")
+    return str(value).strip().lower()
+
+
+class ProfileValidationService:
+    @staticmethod
+    def validate(
+        first_name,
+        last_name,
+        doc,
+        address,
+        neighborhood,
+        city,
+        state,
+        country,
+        address_number=None,
+        address_line_2=None,
+    ):
+        first_name = validate_alphanumeric("first_name", first_name)
+        last_name = validate_alphanumeric("last_name", last_name)
+        doc = validate_alphanumeric("doc", doc)
+        address = validate_alphanumeric("address", address)
+        if address_number is not None:
+            address_number = validate_alphanumeric("address_number", address_number)
+        if address_line_2 is not None:
+            address_line_2 = validate_alphanumeric("address_line_2", address_line_2)
+        neighborhood = validate_alphanumeric("neighborhood", neighborhood)
+        city = validate_alphanumeric("city", city)
+        state = validate_alphanumeric("state", state)
+        country = validate_alphanumeric("country", country)
+
+        return {
+            "first_name": first_name,
+            "last_name": last_name,
+            "doc": doc,
+            "address": address,
+            "address_number": address_number,
+            "address_line_2": address_line_2,
+            "neighborhood": neighborhood,
+            "city": city,
+            "state": state,
+            "country": country,
+        }
